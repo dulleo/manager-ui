81235efd705f26c349aa82fb77c954b100c2e2a4
dulleo <dulleo@gmail.com>
2019-06-21 09:51:46 +0200

    providers added MessageService and DataService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CommunityListComponent } from './community/community-list/community-list.component';
+import { DataService } from './service/data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { CommunityCreateEditComponent } from './community/community-create-edit/community-create-edit.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MessageService } from './service/message.service';
+import { AccountListComponent } from './account/account-list/account-list.component';
+import { AccountCreateEditComponent } from './account/account-create-edit/account-create-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CommunityListComponent,
+    CommunityCreateEditComponent,
+    AccountListComponent,
+    AccountCreateEditComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [DataService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
