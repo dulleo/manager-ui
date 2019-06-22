e8589c679ed50cc316318747b1a2a39a74eee62d
dulleo <dulleo@gmail.com>
2019-06-22 10:43:43 +0200

    doorway impl added

--- src/app/service/message.service.ts
+++ src/app/service/message.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Community } from '../model/community';
 import { BankAccount } from '../model/account';
+import { Doorway } from '../model/doorway';
 
 @Injectable()
 export class MessageService {
@@ -12,6 +13,9 @@ export class MessageService {
     private accountMessageSource = new BehaviorSubject(new BankAccount);
     selectedAccount = this.accountMessageSource.asObservable();
 
+    private doorwayMessageSource = new BehaviorSubject(new Doorway);
+    selectedDoorway = this.doorwayMessageSource.asObservable();
+
     constructor() {}
 
     selectCommunity(community: Community) {
@@ -20,6 +24,10 @@ export class MessageService {
 
     selectAccount(account: BankAccount) {
         this.accountMessageSource.next(account);
+    }
+
+    selectDoorway(doorway: Doorway) {
+        this.doorwayMessageSource.next(doorway);
     }
 
 }
