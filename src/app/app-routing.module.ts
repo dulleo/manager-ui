4ea8d26968db956b479e3409151503bdacddf403
dulleo <dulleo@gmail.com>
2019-06-22 10:50:26 +0200

    doorways routes added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,17 @@ import { CommunityListComponent } from './community/community-list/community-lis
 import { CommunityCreateEditComponent } from './community/community-create-edit/community-create-edit.component';
 import { AccountListComponent } from './account/account-list/account-list.component';
 import { AccountCreateEditComponent } from './account/account-create-edit/account-create-edit.component';
+import { DoorwayListComponent } from './doorway/doorway-list/doorway-list.component';
+import { DoorwayCreateEditComponent } from './doorway/doorway-create-edit/doorway-create-edit.component';
 
 const routes: Routes = [
     { path: ' ', redirectTo: 'communities', pathMatch: 'full'},
     { path: 'communities', component: CommunityListComponent },
     { path: 'communities/create-edit', component: CommunityCreateEditComponent },
     { path: 'accounts', component: AccountListComponent },
-    { path: 'accounts/create-edit', component: AccountCreateEditComponent }
+    { path: 'accounts/create-edit', component: AccountCreateEditComponent },
+    { path: 'doorways', component: DoorwayListComponent },
+    { path: 'doorways/create-edit', component: DoorwayCreateEditComponent }
 ];
 
 @NgModule({
