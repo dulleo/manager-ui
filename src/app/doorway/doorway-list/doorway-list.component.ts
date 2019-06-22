9c884d1bd730ce93fa1f23ce8440baf386a2a681
dulleo <dulleo@gmail.com>
2019-06-22 10:45:27 +0200

    doorway crud impl

---
+++ src/app/doorway/doorway-list/doorway-list.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { Community } from 'src/app/model/community';
+import { Doorway } from 'src/app/model/doorway';
+import { DataService } from 'src/app/service/data.service';
+import { Router } from '@angular/router';
+import { MessageService } from 'src/app/service/message.service';
+
+@Component({
+  selector: 'app-doorway-list',
+  templateUrl: './doorway-list.component.html',
+  styleUrls: ['./doorway-list.component.css']
+})
+export class DoorwayListComponent implements OnInit {
+
+  selectedCommunity: Community;
+  selectedDoorway: Doorway;
+  doorways: Array<Doorway> = [];
+  doorwayExists: Boolean = false;
+  display='none';
+
+  constructor(private dataService: DataService, 
+    private router: Router, 
+    private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.messageService.selectedCommunity.subscribe(message => this.selectedCommunity = message);
+    this.getAllDoorways(this.selectedCommunity.Id);
+  }
+
+  getAllDoorways(communityId:number) {
+    this.dataService.getAllDoorways(communityId).subscribe(resp => {
+      if(resp.ok) {
+        console.log("getAllDoorways response received....");
+        this.doorways = new Doorway().deserializeList(resp.body);
+        if(this.doorways.length > 0) {
+          this.doorwayExists = true;
+        } else {
+          this.doorwayExists = false;
+        }
+      }
+    });
+  }
+
+  createDoorway() {
+    console.log("Create doorway is called....");
+    this.messageService.selectCommunity(this.selectedCommunity);
+    this.messageService.selectDoorway(new Doorway);
+    this.router.navigate(['doorways/create-edit']);
+  }
+
+  updateDoorway(doorway: Doorway) {
+    console.log("Update doorway id: " + doorway.Id);
+    this.messageService.selectDoorway(doorway);
+    this.router.navigate(['doorways/create-edit']);
+  }
+
+  openModal(doorway: Doorway){
+    this.display='block';
+    this.selectedDoorway = doorway; 
+  }
+
+  onCloseHandled(){
+    this.display='none';
+    this.selectedDoorway = null; 
+  }
+
+  onDeleteHandled() {
+    this.dataService.deleteDoorway(this.selectedCommunity.Id, this.selectedDoorway.Id).subscribe(resp => {
+        if(resp.ok) {
+            //alert("Test " + this.selectedTestDTO.Name + " is successfully deleted!");
+            this.getAllDoorways(this.selectedCommunity.Id);
+            this.onCloseHandled();
+        }
+      });
+  }
+
+}
